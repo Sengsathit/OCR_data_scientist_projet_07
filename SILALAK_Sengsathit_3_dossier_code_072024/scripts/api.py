5bea3179deaa7d73cc20ac3a17bf5bdc31568e71
Sengsathit <sengsathit@gmail.com>
2024-09-05 15:49:13 +0200

    Logistic regression

--- SILALAK_Sengsathit_3_dossier_code_072024/scripts/api.py
+++ SILALAK_Sengsathit_3_dossier_code_072024/scripts/api.py
@@ -2,23 +2,52 @@ import os
 import joblib
 import pandas as pd
 import shap
+import pickle
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# Récupérez le répertoire actuel du fichier api.py
+# Répertoire actuel du fichier api.py
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
-@app.route("/scoring", methods=['GET'])
+# Chargement du modèle
+model_path = os.path.join(current_directory, "..", "model_stable", "model.pkl")
+model = joblib.load(model_path)
+
+# Chargement de l'imputer et du scaler
+imputer_path = os.path.join(current_directory, "..", "tools", "imputer.pkl")
+scaler_path = os.path.join(current_directory, "..", "tools", "scaler.pkl")
+with open(imputer_path, 'rb') as imputer:
+    imputer = pickle.load(imputer)
+with open(scaler_path, 'rb') as scaler:
+    scaler = pickle.load(scaler)
+
+# Chargement du dataset
+csv_path = os.path.join(current_directory, "..", "data", "df_train_domain.csv")
+df = pd.read_csv(csv_path)
+
+@app.route("/scoring", methods=['POST'])
 def scoring():
 
-    # Effectuer la prédiction en fonction de l'ID du client
+    data = request.json
+    sk_id_curr = data['SK_ID_CURR']
+
+    # Sélectionner les caractéristiques en fonction de l'ID du client
+    sample = df[df['SK_ID_CURR'] == sk_id_curr]
+    sample = sample.drop(columns=['SK_ID_CURR', 'TARGET'])
+    sample = imputer.transform(sample)
+    sample = scaler.transform(sample)
+    
+    # Prédire 
+    prediction = model.predict_proba(sample)
+    probability = prediction[0][1]
 
     # Calculer les valeurs SHAP pour le client donné
 
     # Retourner le résultat du scoring ainsi que les valeurs SHAP
     return jsonify({
-        'probability': 0.5, 
+        'sk_id_curr': sk_id_curr,
+        'probability': probability, 
         'shap_values': [],
         'feature_names': [],
         'feature_values': []
